f5a92f3b66beb6fc354245e1b4771f602264172e
thewilkybarkid <chriswilkinson84@gmail.com>
2015-04-17 09:41:03 +0100

    Use Behaviors

--- js/questions_and_answers.js
+++ js/questions_and_answers.js
@@ -1,8 +1,12 @@
-jQuery(function ($) {
+(function ($) {
 
-    $('.campl-questions-question').click(function () {
-        $(this).next('.campl-questions-answer').toggleClass('campl-questions-answer-revealed');
-        $(window).trigger('resize'); // force column heights to be recalculated
-    });
+    Drupal.behaviors.cambridgeQuestionsAndAnswers = {
+        attach: function (context, settings) {
+            $('.campl-questions-question', context).click(function () {
+                $(this).next('.campl-questions-answer').toggleClass('campl-questions-answer-revealed');
+                $(window).trigger('resize'); // force column heights to be recalculated
+            });
+        }
+    };
 
-});
+})(jQuery);
